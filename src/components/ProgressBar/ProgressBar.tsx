28757a2473ec8d517c2aadeb0727fffe9e7bd9f4
Nick Grato <nickgrato@ngrato-27545.local>
2022-07-22 13:21:24 -0700

    fixing some component bugs

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 import styles from './ProgressBar.module.scss';
 
 export type ProgressBarPropsT = {
- value : number
+  value: number;
+  classProp?: string;
 };
 
-const ProgressBar = ({
-  value = 40
-}: ProgressBarPropsT) => {
+const ProgressBar = ({ value = 40, classProp = '' }: ProgressBarPropsT) => {
   return (
-    <div className={styles.wrapper}>
+    <div className={`${styles.wrapper} ${classProp}`}>
       <div className={styles.container}></div>
-      <div className={styles.value} style={{width: value + '%'}}></div>
+      <div className={styles.value} style={{ width: value + '%' }}></div>
     </div>
   );
 };
