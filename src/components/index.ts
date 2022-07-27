b19f653f1cd275a5e9bf43305d46dfe0e885f894
Nick Grato <nickgrato@ngrato-27545.local>
2022-07-27 11:57:59 -0700

    updating buttons and adding the copy button

--- src/components/index.ts
+++ src/components/index.ts
@@ -16,3 +16,4 @@ export {
 } from './Input';
 export { default as ProgressBar } from './ProgressBar';
 export { default as RadioButton } from './RadioButton';
+export { default as CopyButton } from './CopyButton';
