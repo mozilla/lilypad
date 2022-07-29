1b7f13254e8014fcc5040016a51447298d1401e5
Nick Grato <nickgrato@ngrato-27545.local>
2022-07-29 13:19:47 -0700

    chaning up the progress bars css format

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -4,13 +4,14 @@ import styles from './ProgressBar.module.scss';
 export type ProgressBarPropsT = {
   value: number;
   classProp?: string;
+  classValueProp?: string;
 };
 
-const ProgressBar = ({ value = 40, classProp = '' }: ProgressBarPropsT) => {
+const ProgressBar = ({ value = 40, classProp = '', classValueProp = ''}: ProgressBarPropsT) => {
   return (
     <div className={`${styles.wrapper} ${classProp}`}>
       <div className={styles.container}></div>
-      <div className={`${styles.value} progress_value`} style={{ width: value + '%' }}></div>
+      <div className={`${styles.value} ${classValueProp}`} style={{ width: value + '%' }}></div>
     </div>
   );
 };
